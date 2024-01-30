2bbc75822ffd5d9765e7c16a8bb1844b8da51c53
Nidhi Chaudhary <106021001+nidhichaudhary2002@users.noreply.github.com>
2024-01-30 23:31:08 +0530

    added MeasurementOfTeeth

---
+++ script.js
@@ -0,0 +1,40 @@
+function toRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
+
+function calculateInvalidInput() {
+  // Get user input values
+  var pressureAngle = toRadians(
+    parseFloat(document.getElementById('pressureAngle').value)
+  );
+  var helixAngle = toRadians(
+    parseFloat(document.getElementById('helixAngle').value)
+  );
+
+  // Perform calculations
+  var temp = Math.tan(pressureAngle) / Math.cos(helixAngle);
+  var y = Math.atan(temp);
+
+  var invalidInput = temp - y;
+
+  return invalidInput;
+}
+
+function MeasurementOfTeeth() {
+  var module = parseFloat(document.getElementById('module').value);
+  var pressureAngle = toRadians(
+    parseFloat(document.getElementById('pressureAngle').value)
+  );
+
+  var z2 = parseFloat(document.getElementById('z2').value);
+  var z1 = parseFloat(document.getElementById('z1').value);
+
+  var invalidInput = calculateInvalidInput();
+  console.log(invalidInput);
+
+  let ans =
+    module * Math.cos(pressureAngle) * (Math.PI * z2 + z1 * invalidInput);
+
+  document.getElementById('result').innerHTML =
+    'Measurement of Teeth: ' + ans.toFixed(5);
+}
