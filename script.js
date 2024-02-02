43e7e7e581451605fd6f260a346ac2ea5dc0b319
Nidhi Chaudhary <106021001+nidhichaudhary2002@users.noreply.github.com>
2024-02-02 13:18:30 +0530

    added toggle between Spur and helical

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ function toRadians(degrees) {
   return degrees * (Math.PI / 180);
 }
 
+function toggle() {
+  var currentState = document.getElementById('toggleButton');
+  if (currentState.innerHTML == 'Helical') {
+    currentState.innerHTML = 'Spur';
+    return true;
+  } else {
+    currentState.innerHTML = 'Helical';
+    return false;
+  }
+}
+
 function calculateInvalidInput() {
-  // Get user input values
-  var pressureAngle = toRadians(
-    parseFloat(document.getElementById('pressureAngle').value)
-  );
-  var helixAngle = toRadians(
-    parseFloat(document.getElementById('helixAngle').value)
-  );
+ 
+  var isHelical = toggle();
+  if (!isHelical) {
+    return 0.014904383;
+  } else {
+    var pressureAngle = toRadians(
+      parseFloat(document.getElementById('pressureAngle').value)
+    );
+    var helixAngle = toRadians(
+      parseFloat(document.getElementById('helixAngle').value)
+    );
 
-  // Perform calculations
-  var temp = Math.tan(pressureAngle) / Math.cos(helixAngle);
-  var y = Math.atan(temp);
-  var invalidInput = temp - y;
+    // Perform calculations
+    var temp = Math.tan(pressureAngle) / Math.cos(helixAngle);
+    var y = Math.atan(temp);
+    var invalidInput = temp - y;
 
-  return invalidInput;
+    return invalidInput;
+  }
 }
 
 function MeasurementOfTeeth() {
@@ -29,8 +45,6 @@ function MeasurementOfTeeth() {
   var z1 = parseFloat(document.getElementById('z1').value);
 
   var invalidInput = calculateInvalidInput();
-  console.log(invalidInput);
-
   let ans =
     module * Math.cos(pressureAngle) * (Math.PI * z2 + z1 * invalidInput);
 
@@ -44,7 +58,7 @@ function PCD() {
   var helixAngle = toRadians(
     parseFloat(document.getElementById('helixAngle').value)
   );
-  ans = (z1 * module) / helixAngle;
+  ans = (z1 * module) / Math.cos(helixAngle);
 
   document.getElementById('ansDiv').innerHTML = 'PCD : ' + ans.toFixed(5);
   return ans;
@@ -56,5 +70,4 @@ function helixAngle() {
   var helixAngle = toRadians(
     parseFloat(document.getElementById('helixAngle').value)
   );
-
 }
